d0118e163c38149cdde8179e2c90c7155fc86b0d
Jason Liu <jl532@duke.edu>
2017-11-30 11:59:33 -0500

    use nested for loops to ID best parameters for circle ID
    nested for loops... only with param1 and param2. can test other parameters.

--- circleID_optimization.py
+++ circleID_optimization.py
@@ -12,59 +12,47 @@ def mouseLocationClick(event,x,y,flags,param):
 def pullElementsFromList(datList,argument): # use this when you have a 2d list and want a specific element from each entry
     return [thatSpecificArgument[argument] for thatSpecificArgument in datList]
 
-def circleIDMultivariableOptimization(imageOfCirclesInArray,targetNumberOfCircles):
-    medianBlurArg = 3 # sliding window size, it's a moving averager to remove some random noise
-    circleAlgoImageSmoothed = cv2.medianBlur(imageOfCirclesInArray,medianBlurArg)
-        
-    HoughCircDP = 1 # don't mess with this for now
-    HoughCircMinDist = 25 # the minimum distance between centers of circles (in pixels)
-    HoughCircParam1 = 40 # don't mess with this for now, it's used for edge detection
-    HoughCircParam2 = 15 # the smaller this is, the more circles will be detected (including false ones) and the larger this is, the more circles will be potentially returned. Test this though.
-    HoughCircMinRadius = 14 # the lower limit of detected circle radius (in pixels). capture spots (generated from the genepix) usually are around 14-16 pixels in radius
-    HoughCircMaxRadius = 30 # the upper limit of detected circle radius (in pixels)
-    
-    houghParamList = [HoughCircDP, HoughCircMinDist, HoughCircParam1, HoughCircParam2, HoughCircMinRadius, HoughCircMaxRadius]
-        
-    circlesRaw = cv2.HoughCircles(circleAlgoImageSmoothed,cv2.cv.CV_HOUGH_GRADIENT,houghParamList[0],houghParamList[1],param1=houghParamList[2],param2=houghParamList[3],minRadius=houghParamList[4],maxRadius=houghParamList[5])
-    circles = np.uint16(np.around(circlesRaw))
-    circleCount = len(circles[0])
-    if circleCount is not targetNumberOfCircles:
-        circleIDLoopBool = True
-    else:
-        return circles
-    while circleIDLoopBool:
-        #begin to perturb the model variables to identify parameters required to find the right number of circles
-        # figure out algorithmically how best to perturb
-    
+startTime = time.time()
+
 fileNameInput = "62,5.tif"
 targetNumberOfSpots = 149
+imageOfCirclesInArray = cv2.imread(fileNameInput,0) # import the raw image here, currently set as "0,488.tif"
 
 medianBlurArg = 3 # sliding window size, it's a moving averager to remove some random noise
+circleAlgoImageSmoothed = cv2.medianBlur(imageOfCirclesInArray,medianBlurArg)
+    
 HoughCircDP = 1 # don't mess with this for now
 HoughCircMinDist = 25 # the minimum distance between centers of circles (in pixels)
 HoughCircParam1 = 40 # don't mess with this for now, it's used for edge detection
 HoughCircParam2 = 15 # the smaller this is, the more circles will be detected (including false ones) and the larger this is, the more circles will be potentially returned. Test this though.
 HoughCircMinRadius = 14 # the lower limit of detected circle radius (in pixels). capture spots (generated from the genepix) usually are around 14-16 pixels in radius
 HoughCircMaxRadius = 30 # the upper limit of detected circle radius (in pixels)
-numberOfCaptureSpots = 5
- 
-startTime = time.time()
 
-imgRaw = cv2.imread(fileNameInput,0) # import the raw image here, currently set as "0,488.tif"
-imgsmooth = cv2.medianBlur(imgRaw,medianBlurArg) # low pass filter the image, blurring a pixel with a 3 pixel sliding window
-cimg = cv2.cvtColor(imgRaw,cv2.COLOR_GRAY2BGR) #converts raw image to grayscale
-verificationImg = cimg.copy();
+houghParamList = [HoughCircDP, HoughCircMinDist, HoughCircParam1, HoughCircParam2, HoughCircMinRadius, HoughCircMaxRadius]
 
 # this is the line of code that finds the circles in an image. more information about the parameters can be found at:
 # https://www.pyimagesearch.com/2014/07/21/detecting-circles-images-using-opencv-hough-circles/
+    
+rangeOfComparison = 10
 
-circles = cv2.HoughCircles(imgsmooth,cv2.cv.CV_HOUGH_GRADIENT,HoughCircDP,HoughCircMinDist,param1=HoughCircParam1,param2=HoughCircParam2,minRadius=HoughCircMinRadius,maxRadius=HoughCircMaxRadius)
+rangeOfParams1 = range(HoughCircParam1 - rangeOfComparison, HoughCircParam1 + rangeOfComparison + 1)
+rangeOfParams2 = range(HoughCircParam2 - rangeOfComparison, HoughCircParam2 + rangeOfComparison + 1)
 
-# rounds and typecasts the circle information into unsigned 16 bit integers 
-circles = np.uint16(np.around(circles))
+iterationNumber = 1 
+for eachParam1 in rangeOfParams1:
+    for eachParam2 in rangeOfParams2:
+        circlesRaw = cv2.HoughCircles(circleAlgoImageSmoothed,cv2.cv.CV_HOUGH_GRADIENT,houghParamList[0],houghParamList[1],param1=eachParam1,param2=eachParam2,minRadius=houghParamList[4],maxRadius=houghParamList[5])
+        circles = np.uint16(np.around(circlesRaw))
+        circleCount = len(circles[0])
+        
+        print( "iteration Number: " + str(iterationNumber) + " param1: " + str(eachParam1) + " param2: " + str(eachParam2) + "  ||| difference in ID  " + str(targetNumberOfSpots - circleCount) )
+        iterationNumber = iterationNumber + 1
+        if circleCount == targetNumberOfSpots:
+            print("------set of parameters found: param1: " + str(eachParam1) + " param2: " + str(eachParam2))
+        
 
-circleCount = len(circles[0])
-
+    
+         
 finishedTime = time.time() - startTime
 
 print("done, with " + str(circleCount) + " circles identified in " + str(finishedTime) + " seconds") # prints out the number of identified circles for debugging purposes.
